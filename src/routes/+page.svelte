4d1bcc6c8053f7389454816ff10ba334d54efa06
Aloun <alounv@gmail.com>
2022-09-30 18:24:54 +0200

    Adds basic functionnalities

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,9 @@
 	}
 	let games: GamePreview[] = [];
 	let newGameName = '';
+	let ws: WebSocket;
+	let message = '';
+	let name = ''; // save name somewhere ? localStorage ? store ?
 
 	const refreshList = async () => {
 		const response = await fetch('http://localhost:5000/games/all', {
@@ -32,6 +35,7 @@
 
 	const deleteGame = async (gameId: number) => {
 		await fetch(`http://localhost:5000/games/delete?id=${gameId}`, {
+			// should be games/id/delete
 			method: 'DELETE',
 			headers: {
 				'content-type': 'application/json'
@@ -41,6 +45,27 @@
 		refreshList();
 	};
 
+	const joinGame = async (gameId: number, playerName: string) => {
+		ws = new WebSocket(`ws://localhost:5000/games/${gameId}/join?playerName=${playerName}`);
+
+		ws.onclose = () => {
+			console.log('closed');
+			refreshList();
+		};
+
+		ws.onerror = () => {
+			console.error('WS error');
+			refreshList();
+		};
+
+		ws.onmessage = async (event) => {
+			const data = await event.data.text();
+			console.log(data);
+			message = data;
+			refreshList();
+		};
+	};
+
 	onMount(() => {
 		refreshList();
 	});
@@ -48,13 +73,20 @@
 
 <h1>Test connection to server</h1>
 
-<button on:click={refreshList}>Refresh list</button>
+<h2>Your name is: {name}</h2>
 <div>
-	<form on:submit|preventDefault={createGame}>
-		<label>Create new game</label>
-		<input bind:value={newGameName} />
-	</form>
+	<label htmlfor="name">Set name</label>
+	<input id="name" bind:value={name} />
 </div>
+
+<button on:click={refreshList}>Refresh list</button>
+
+<form on:submit|preventDefault={createGame}>
+	<label htmlFor="create-game">Create new game</label>
+	<input id="create-game" bind:value={newGameName} />
+</form>
+
+<div style="color: red;">{message}</div>
 
 <ul>
 	{#each games as game}
@@ -65,6 +97,7 @@
 			<span>{game.Phase}</span>
 			<span>{game.Players}</span>
 			<button on:click={() => deleteGame(game.ID)}>Delete</button>
+			<button on:click={() => joinGame(game.ID, name)}>Join</button>
 		</li>
 	{/each}
 </ul>
