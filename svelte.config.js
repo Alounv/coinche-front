a97c77cbe8765edafe5ba7a90b45e5e8e0fa8e5f
Aloun <alounv@gmail.com>
2022-10-07 17:07:03 +0200

    Adds vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,16 @@
-import adapter from 'svelte-adapter-bun';
+//import adapter from 'svelte-adapter-bun';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			edge: false,
+			external: [],
+			split: false
+		})
 	}
 };
 
