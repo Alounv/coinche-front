39c6784569138055b2a6816f27d09c0444dafd93
Aloun <alounv@gmail.com>
2022-10-08 15:08:12 +0200

    Change .ENV

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,5 @@
 export const variables = {
 	IS_ADMIN: import.meta.env.VITE_ADMIN === 'true',
-	REST_URL: import.meta.env.VITE_SERVER_SCHEME + '://' + import.meta.env.VITE_SERVER_HOST,
-	WS_URL: 'ws://' + import.meta.env.VITE_SERVER_HOST
+	REST_URL: import.meta.env.VITE_REST_URL,
+	WS_URL: import.meta.env.VITE_WS_URL
 };
